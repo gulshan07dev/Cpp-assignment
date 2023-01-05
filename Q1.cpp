6470a09b054f2abcf040c54a16ee4f3e1cc6f5fb
Gulshan kumar <gulshanku9135@gmail.com>
2023-01-05 13:39:39 +0530

    assignment 5 (Number system)

--- Q1.cpp
+++ Q1.cpp
@@ -1,16 +1,23 @@
-//Q1- Write a program to calculate factorial of a number
-
+// Q1 - Print the sum of the first 5 decimal numbers from 1 to 5 in binary format.
 
 #include<iostream>
 using namespace std;
-int main() {
-     cout<<"Enter Number: ";
-     int number;
-     cin>>number;
-     int factorial=1;
-     for(int i = 1; i <= number; i++){
-          factorial *= i;
+int main(){
+
+     int sum=0;
+      for(int i=1;i<=5;i++){
+          sum+=i;
      }
-     cout<<"Factorial of "<<number<<" is: "<<factorial;
+     int power=1;
+     int ans=0;
+     
+     while(sum>0){
+          int paritydigit=sum%2;
+          ans+=paritydigit*power;
+          power*=10;
+          sum/=2;
+     }
+     cout<<ans<<endl;
+
      return 0;
 }
--- Q2.cpp
+++ Q2.cpp
@@ -1,24 +1,24 @@
-//Q2- Write a program to print all Armstrong numbers between 100 to 500.
+/*  Q2 - Given a natural number, find the number of 0’s in the binary 
+representation of that number    */
 
 #include<iostream>
 using namespace std;
-int main(){
+int main() {
 
-cout<<"Armstrong numbers in the range of 100 to 500 are :"<<endl;
- for(int i=0;i<500;i++)
- {
-int sum = 0;
-int t = i;
-while(t!=0)
-{
-sum = sum+((t%10)*(t%10)*(t%10));
-t = t/10;
+     int n;
+     cout<<"Enter the number: ";
+     cin>>n;
+     int count = 0;
+     
+     while(n > 0){
+          int parity = n%2;
+          if(parity == 0){
+               count++;
+          }
+          n /= 2;
 }
-if(sum == i)
-{
-cout << i <<"\n";
-}
- }
-
+         
+     cout<<"The Number of Zeros is: "<<count<<endl;
+     
      return 0;
 }
